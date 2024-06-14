18db8a1ae9cda47c36bf522bba4d9e923d9841a5
Avijit Sarkar <avijit.a.sarkar@capgemini.com>
2024-06-15 00:31:36 +0530

    next auth and modifying  the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
-import React from 'react'
+import React from 'react';
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 export default function Header() {
+  const { data: session } = useSession();
+  console.log('session : ', session);
+
   return (
     <div className="shadow-sm border-b sticky top-0">
       <div className="flex justify-between items-center max-w-6xl mx-auto bg-white z-30 p-3">
@@ -28,7 +34,24 @@ export default function Header() {
         <input type="text" placeholder="Search" className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]" />
 
         {/* menu items */}
-        <button className="text-sm font-semibold text-blue-500">Log In</button>
+
+        {
+          session ? (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img
+              src={session.user.image}
+              alt={session.user.name}
+              className="h-10 w-10 rounded-full cursor-pointer"
+              onClick={signOut}
+            />
+          ) : (
+              <button
+                onClick={() => signIn()}
+                className="text-sm font-semibold text-blue-500">
+                Log In
+              </button>
+          )
+        }
       </div>
     </div>
   )
